85043afbad230484bc1038e3af8672d975f881a9
Octavia Cephas <octavia.cephas@gmail.com>
2018-03-19 22:45:48 -0400

    css edits and local storage javascript

--- public/assets/js/javascript.js
+++ public/assets/js/javascript.js
@@ -1,9 +1,9 @@
 $("#create").click(function(event) {
   event.preventDefault();
   const sendData = {
-    name: $("#inputEmail4").val(),
+    name: $("#inputName").val(),
     password: $("#inputPassword4").val(),
-    gender: $("#inputStyle").find("option:selected").text(),
+    gender: $("#inputGender").find("option:selected").text(),
     email: $("#inputEmail4").val(),
     zip: $("#inputZip").val(),
   };
@@ -12,4 +12,19 @@ $("#create").click(function(event) {
   .then(function(data) {
     console.log("Check database");
   })
-})
+ 
+  $("#name-display").text(name);
+
+  sessionStorage.clear();
+
+  sessionStorage.setItem("name", name);
+  sessionStorage.setItem("email", email);
+  sessionStorage.setItem("password", password);
+  sessionStorage.setItem("gender", gender);
+  sessionStorage.setItem("zip", zip);
+
+});
+
+ $("#name-display").text(sessionStorage.getItem("name"));
+
+
