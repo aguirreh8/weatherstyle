f8422b30f73f26e4d4316cdedbf170b411dd038c
Octavia Cephas <octavia.cephas@gmail.com>
2018-03-22 20:26:19 -0400

    modal and ebay icon

--- public/assets/js/javascript.js
+++ public/assets/js/javascript.js
@@ -1,30 +1,37 @@
-$("#create").click(function(event) {
-  event.preventDefault();
-  const sendData = {
-    name: $("#inputName").val(),
-    password: $("#inputPassword4").val(),
-    gender: $("#inputGender").find("option:selected").text(),
-    email: $("#inputEmail4").val(),
-    zip: $("#inputZip").val(),
-  };
+ $(document).ready(function() { 
+  $("#create").click(function(event) {
+    event.preventDefault();
+    const sendData = {
+      name: $("#inputName").val(),
+      password: $("#inputPassword4").val(),
+      gender: $("#inputGender").find("option:selected").text(),
+      email: $("#inputEmail4").val(),
+      zip: $("#inputZip").val(),
+    };
 
-  $.post("/api/newUser", sendData)
-  .then(function(data) {
-    console.log("Check database");
-  })
- 
-  $("#name-display").text(name);
+    // $.post("/api/newUser", sendData)
+    // .then(function(data) {
+    //   console.log("Check database");
+    // })
+   
+    $("#name-display").text(name);
 
-  sessionStorage.clear();
+    sessionStorage.clear();
 
-  sessionStorage.setItem("name", name);
-  sessionStorage.setItem("email", email);
-  sessionStorage.setItem("password", password);
-  sessionStorage.setItem("gender", gender);
-  sessionStorage.setItem("zip", zip);
+    sessionStorage.setItem("name", name);
+    sessionStorage.setItem("email", email);
+    sessionStorage.setItem("password", password);
+    sessionStorage.setItem("gender", gender);
+    sessionStorage.setItem("zip", zip);
 
-});
+    $('#myModal').modal({
+      backdrop: "show"
+    })
 
+    $(".modal-backdrop").addClass('fade in');
+
+  });
+})
  $("#name-display").text(sessionStorage.getItem("name"));
 
 
