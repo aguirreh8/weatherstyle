b6ca90e8fc723b372c5f92d1bae6a4e5ec778ae6
unknown <aguirreh8@gmail.com>
2018-05-30 20:09:59 -0400

    Change DB connection to local database

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,15 @@
 // Require mysql connection
 const mysql = require("mysql");
 
-// let connection = "";
-
 // If deployed, used JAWSDB to connect to database
-if (process.env.JAWSDB_URL) {
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-	connection = mysql.createConnection({
-		port: 8889,
-		host: "localhost",
-		user: "root",
-		password: "root",
-		database: "weather_style_db"
-	})
-}
+const connection = mysql.createConnection({
+	port: 8889,
+	host: "localhost",
+	user: "root",
+	password: "root",
+	database: "weather_style_db"
+})
+
 // Connect to database
 connection.connect(function(err) {
 	if (err) {
